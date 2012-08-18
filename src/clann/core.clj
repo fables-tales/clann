18aa52d796dd4b44b22386220c8b3d85fed11345
Sam Phippen <samphippen@googlemail.com>
2012-08-18 18:27:06 +0100

    Getting somewhere!
    It can now solve for a 2 input, no hidden layer, single
    output case. To describe this as amazeballs would probably
    be just about correct. Soon it will support multi-layer back
    propagation. ZOMG
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/clann/core.clj
+++ src/clann/core.clj
@@ -1,5 +1,7 @@
 (ns clann.core)
 
+(use 'clojure.contrib.generic.math-functions)
+
 (defn zip [a,b]
  (map vector a b))
 
@@ -7,10 +9,82 @@
  (reduce + a))
 
 (defn sigmoid [v]  
- (Math/tanh (/ v 2)))
+ (/ 1.0 (inc (exp (- v)))))
+
+(defn inverseSigmoid [v]
+  (-(log (dec (/ 1.0 v)))))
 
 (defn multiply-pair [pair]
  (* (first pair) (second pair)))
 
 (defn computeWeightSum [inputs, weights]
  (sum (map multiply-pair (zip inputs weights))))
+
+
+(defn activation [inputs, weights]
+  (sigmoid (computeWeightSum inputs weights)))
+
+(defn makeWeights [nWeights]
+  (take nWeights (repeatedly #(- 1 (rand 2)))))
+
+(defn makeNetworkLayerWeights [nInputs, nOutputs]
+  (take nOutputs (repeatedly #(makeWeights nInputs))))
+
+(defn feedForward [inputs, layerWeights]
+  (map #(activation inputs %1) layerWeights))
+
+
+(defn twoLayerPerceptron [inputs, layer1Weights, layer2Weights]
+  (feedForward (feedForward inputs layer1Weights) layer2Weights))
+
+
+(def xorExample [[0,0],[0,1],[1,0][1,1]])
+(def xorLabels (map #([apply bit-xor %1]) xorExample))
+
+(defn diff [value1, value2]
+  (- value1 value2))
+
+(defn ones [n]
+  (take n (repeatedly (fn [] 1))))
+
+(defn neuronError [thisNeuronExpected, thisNeuronActual]
+  (* thisNeuronActual 
+     (- 1 thisNeuronActual) 
+     (- thisNeuronExpected thisNeuronActual)))
+
+(defn neuronWeightsDelta [thisNeuronExpected, 
+                          thisNeuronActual, 
+                          previousLayerActivations]
+  (map #(* (neuronError thisNeuronExpected thisNeuronActual) %1) 
+          previousLayerActivations))
+
+(defn newNeuronWeights [weights, delta]
+  (map #(+ (first %1) (second %1)) (zip weights delta)))
+
+
+(def weights1 (makeNetworkLayerWeights 2 1))
+(def sample [(sigmoid 1),(sigmoid 0)])
+(def label [1])
+(def layer1results (feedForward sample weights1))
+
+
+(defn newWeights [e,
+                  a,
+                  act,
+                  weights]
+  (newNeuronWeights weights (neuronWeightsDelta e a act)))
+
+
+(defn nextWeights [previousWeights,
+                            sample,
+                            label]
+  (let [answer (feedForward sample previousWeights)]
+    (newWeights (first label) 
+                (first answer) 
+                sample 
+                (first previousWeights))))
+
+
+
+(defn doEpochs [n]
+  (last (take n (iterate (fn [weights] [(nextWeights weights sample label)]) weights1))))
--- test/clann/test/core.clj
+++ test/clann/test/core.clj
@@ -2,15 +2,35 @@
   (:use [clann.core])
   (:use [clojure.test]))
 
-(deftest sigmoid-of-zero 
-  (is 0 (sigmoid 0))) 
-
-(deftest sigmoid-of-1
-  (is true (< 0.0001 (Math/abs (- (sigmoid 1) 0.4621117)))))
-
+(deftest sigmoid-inverse-sigmoid
+         (is (> 0.000001 (Math/abs (- 0.5 (inverseSigmoid (sigmoid 0.5))))))
+         (is (> 0.000001 (let [r (rand)] (- r (inverseSigmoid (sigmoid r)))))))
 
 (deftest test-sum
- (is 7 (sum '(1,2,4))))
+ (is (= 7 (sum '(1,2,4)))))
 
 (deftest weight-activation
- (is 14 (computeWeightSum '(1,2,4) '(9,2,0.25))))
+ (is (= 14 (computeWeightSum '(1,2,4) '(9,2,0.25)))))
+
+(deftest activation-test
+         (is (= 1 (activation '(100) '(100))))
+         (is (= 0.5 (activation '(0) '(0))))
+         (is (< 0.5 (activation '(1) '(2))))
+         (is (< 0.5 (activation '(1) '(0.3)))))
+
+(deftest make-weights-test
+         (is (= 10 (count (makeWeights 10)))))
+
+(deftest make-network-layer-weights-test
+         (is (= 3 (count (makeNetworkLayerWeights 1 3))))
+         (is (= 1 (count (first  (makeNetworkLayerWeights 1 3)  ))))
+         (is (= 1 (count (second (makeNetworkLayerWeights 1 3)  ))))
+         (is (= 1 (count (nth    (makeNetworkLayerWeights 1 3) 2)))))
+
+(deftest feed-forward-test
+         (is (= [1,1] (feedForward [1,2] [[100,0],[0,50]])))
+         (is (= [0.5, 0.5] (feedForward [1,2] [[0,0],[0,0]]))))
+
+
+(deftest propagate-simple-test
+         (is (> 0.99 (first (feedForward sample (doEpochs 1000))))))
